a4fbd9bdb945d00cbac86234627fac5af84e79d7
Finn Navin <thomasfnavin@gmail.com>
2018-05-05 14:36:06 -0400

    Made the Exit Button stay on the right side, but no animation

--- main.js
+++ main.js
@@ -3,20 +3,17 @@ var frame = document.getElementById("frame");
 var snakeB = document.getElementById("snakeB");
 var scalingSnakeB = document.getElementById("scalingSnakeB");
 var overlay = document.getElementById("overlay");
-var exit = document.getElementById("closeSidebar");
+var exitB = document.getElementById("exitButton");
 function openSidebar(link){
   overlay.style.display = "block";
   frame.src=link;
   var pos = 0;
-  var run = setInterval(anim, 10);
-
-  function setwidth(){ sidebar.style.swidth = 100 - parseInt(sidebar.style.left) + "%" }
-
+  var run = setInterval(anim, 2);
   function anim() {
     if(pos >= Math.PI){ // Alternatively, if(sidebar.style.left == 15%)
       clearInterval(run);
       frame.style.src=link;
-      setwidth();
+      exitB.style.display = "block";
     } else{
     sidebar.style.left = ((15) +  85 * (Math.cos(pos) + 1)/2 + "%"); // Plug it into Desmos to see how it works.
     pos += Math.PI / 200;
@@ -25,6 +22,7 @@ function openSidebar(link){
 }
 function closeSidebar(){;
   overlay.style.display = "none";
+  exitB.style.display = "none";
   var pos = 0;
   var run = setInterval(anim, 1);
   function anim(){
@@ -43,4 +41,4 @@ function closeSidebar(){;
 snakeB.onclick = function(){openSidebar("https://finncowbell.github.io/snake/")};
 scalingSnakeB.onclick = function(){openSidebar("https://finncowbell.github.io/scalingSnake")}
 overlay.onclick = function(){closeSidebar();}
-exit.onclick = function(){closeSidebar();}
+exitB.onclick = function(){closeSidebar();}
