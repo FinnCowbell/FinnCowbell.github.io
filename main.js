271a2625b04b22cadf68c58a407eccf2852bc199
Finn Navin <thomasfnavin@gmail.com>
2018-07-06 21:52:37 -0400

    -Images Folder -Draggable Sidebar

--- main.js
+++ main.js
@@ -1,12 +1,16 @@
 var sidebar = document.getElementById("sidebar");
 var frame = document.getElementById("frame");
+var frameBlocker= document.getElementById('frameBlocker');
 var snakeB = document.getElementById("snake1");
 var scalingSnakeB = document.getElementById("snake2");
 var saucyB = document.getElementById('saucy');
 var overlay = document.getElementById("overlay");
 var exitB = document.getElementById("exitButton");
+var handle = document.getElementById('handle')
 var opening = false;
+var css = document.getElementById('html').style;
 function openSidebar(link, alpha){
+  sidebar.style.animationName = "openSidebar";
   sidebar.style.display = 'block';
   sidebar.style.animationPlayState = "running";
   overlay.style.display = 'block';
@@ -16,27 +20,41 @@ function openSidebar(link, alpha){
   opening = true;
 }
 function closeSidebar(){;
+  sidebar.style.animationName = "openSidebar";
   sidebar.style.animationPlayState = "running";
   sidebar.style.animationDirection = 'reverse';
   exitB.style.display = 'none';
-  frame.style.src="";
   opening = false;
 }
 sidebar.addEventListener("animationiteration", function(){
+  //when the animation completes
   sidebar.style.animationPlayState = "paused";
   if(opening){
-    sidebar.style.animationDirection = 'reverse';
-    sidebar.style.left = '20%';
+    frame.focus();
+    sidebar.style.animationName = "unset";
     exitB.style.display = 'block';
   } else{
+    sidebar.style.animationName = "unset";
     sidebar.style.display = 'none';
-    sidebar.style.animationDirection = 'normal';
-    sidebar.style.left = "100%";
-    frame.style.src="";
     overlay.style.display = 'none';
 }
 },false);
-
+handle.onmousedown = function(event){
+  frameBlocker.style.display = 'block'; //'blocks' the mouse from focusing the iframe when dragging
+  let newPercent = event.pageX / window.innerWidth * 100 + "%";
+  document.onmousemove = function(event){
+    newPercent = event.pageX / window.innerWidth * 100 + "%";
+    newPercent = parseInt(newPercent) > 90? '90%' : parseInt(newPercent) < 1 ? '1%' : newPercent;
+    sidebar.style.left = newPercent;
+  }
+  document.onmouseup = function(event){
+    css.setProperty('--open-location',(newPercent))
+    frameBlocker.style.display = 'none';
+    frame.focus();
+    document.onmouseup = null;
+    document.onmousemove = null;
+  }
+}
 snakeB.onclick = function(){openSidebar("https://finncowbell.github.io/snake/",.5)};
 scalingSnakeB.onclick = function(){openSidebar("https://finncowbell.github.io/scalingSnake",.5)}
 saucyB.onclick = function(){openSidebar("https://finncowbell.github.io/saucy",.95)}
