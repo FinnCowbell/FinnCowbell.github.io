5c43cc5889305bcfd2203b0cdc251d76a68d8797
Finn Navin <thomasfnavin@gmail.com>
2018-07-16 19:51:44 -0400

    changed nothing (Or did I?)

--- konami.js
+++ konami.js
@@ -1,7 +1,7 @@
 //There's no vanilla-js search array-in-array function! Strings it is.
 let Konami = function(){
    this.sound =
-   this.code = "3838404037393739666513";
+   this.code = "38384040373937396665";
    this.recorded = "";
    this.activateFunction = function(){console.log("activated")};
    this.run =function(){
@@ -15,7 +15,6 @@ let Konami = function(){
       }
       konami.recorded = konami.code.indexOf(konami.recorded) == 0 ? konami.recorded : ""
       //I dont want to make a keylogger. if the logging doesn't match the constant then fuggedaboutit
-      console.log(konami.recorded)
     })}
   }
 //  this.activated = partial(completed,param,.95);
